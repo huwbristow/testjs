1e63fa8b5a3ecce04e848ab18f95ebed74411f9d
huwbristow <45852702+huwbristow@users.noreply.github.com>
2018-12-13 15:48:33 +0000

    Update testjs.js

--- testjs.js
+++ testjs.js
@@ -1,4 +1,8 @@
-src="https://api.apiary.io/seeds/embed.js"
+
+    var script = document.createElement("script"); //Make a script DOM node
+    script.src = "https://api.apiary.io/seeds/embed.js"; //Set it's src to the provided URL
+    document.head.appendChild(script); //Add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+
   var embed = new Apiary.Embed({
     subdomain: 'linkpluspublicapi',
     element: '#embedHere'
